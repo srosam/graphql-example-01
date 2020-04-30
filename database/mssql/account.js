4d8ddab901a71040cd47efdad172650a4d42b449
SteveRosam <srosam@glasswallsolutions.com>
2020-04-30 09:53:47 +0100

    fixes

--- database/mssql/account.js
+++ database/mssql/account.js
@@ -13,7 +13,7 @@ function getAllRecords(config, sql){
             return result["recordset"];
         }).catch(err => {
             console.log(err);
-        }).then(res=>{ return res;});
+        });
 }
 
 function getFirstRecord(config, sql){
@@ -25,7 +25,19 @@ function getFirstRecord(config, sql){
             return result["recordset"][0];
         }).catch(err => {
             console.log(err);
-        }).then(res=>{ return res;});
+        });
+}
+
+function insert(config, sql){
+    return mssql.connect(config['connectionString'])
+        .then(pool => {
+            return pool.request().query(sql);
+        }).then(result => {
+            console.log(result);
+            return true;
+        }).catch(err => {
+            console.log(err);
+        });
 }
 
 module.exports = config => {
@@ -41,6 +53,12 @@ module.exports = config => {
         },
         getFirstByOwnerId(id) {
             return getFirstRecord(config, `SELECT TOP 1 * FROM ACCOUNTS WHERE OwnerId='${id}'`)
+        },
+        add(id, description, type, ownerId){
+            return insert(config, `INSERT INTO ACCOUNTS (id, description, type, ownerId) VALUES ('${id}', '${description}', ${type}, '${ownerId}')`)
+            .then(() => {
+                return this.get(id);
+            })
         }
     }
 }
--- database/mssql/owner.js
+++ database/mssql/owner.js
@@ -13,7 +13,7 @@ function getAllRecords(config, sql){
             return result["recordset"];
         }).catch(err => {
             console.log(err);
-        }).then(res=>{ return res;});
+        });
 }
 
 function getFirstRecord(config, sql){
@@ -25,16 +25,44 @@ function getFirstRecord(config, sql){
             return result["recordset"][0];
         }).catch(err => {
             console.log(err);
-        }).then(res=>{ return res;});
+        });
+}
+
+
+function insert(config, sql){
+    return mssql.connect(config['connectionString'])
+        .then(pool => {
+            return pool.request().query(sql);
+        }).then(result => {
+            return true;
+        }).catch(err => {
+            console.log(err);
+        });
 }
 
 module.exports = config => {
+    
+    function foo(id){
+        return getFirstRecord(config, `SELECT TOP 1 * FROM OWNERS WHERE Id='${id}'`)
+        .then((x)=>{
+                console.log("foo done");
+                return x;
+        });
+
+    }
+    
     return {
         getAll() {
             return getAllRecords(config, "SELECT * FROM OWNERS");
         },
         get(id) {
-            return getFirstRecord(config, `SELECT TOP 1 * FROM OWNERS WHERE Id='${id}'`)
+            return foo(id);
+        },
+        add(id, name, address){
+            return insert(config, `INSERT INTO OWNERS (id, name, address) VALUES ('${id}', '${name}', '${address}')`)
+            .then((res)=>{
+                return foo(id);
+            });
         }
     }
 }
